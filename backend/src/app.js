9acef008a2cc7b43ad90e091b9fd9bd5b36e3232
lilo <williamcco@gmail.com>
2020-02-28 14:03:41 -0300

    multer for file uploading and new file entity

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
+import { resolve } from 'path';
 
 import routes from './routes';
 
@@ -19,6 +20,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
+// File uploading DOES NOT REQUIRE authentication
+// routes.post('/files', upload.single('file'), FileController.store);
 
 // Authentication middleware for the following routes
 routes.use(authMiddleware);
+
+// File uploading DOES REQUIRES authentication
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
